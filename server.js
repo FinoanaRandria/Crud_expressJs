023d9766ffef9396755548a532230fe7bf58f194
FinoanaRandria <fitiavanarex4@gmail.com>
2023-02-03 16:21:40 +0300

    crud 1

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 4333
 
+app.use("/css",express.static('./public'))
 app.set('view engine', 'ejs')
 app.get('/',(req,res)=>{
     res.render('index')
