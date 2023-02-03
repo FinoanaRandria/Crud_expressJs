8f205ad10ee3344783df0e2fac11c4ec96440642
FinoanaRandria <fitiavanarex4@gmail.com>
2023-02-03 16:08:55 +0300

    Crud

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+const port = 4333
+
+app.set('view engine', 'ejs')
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.listen(port,console.log(`Server is runing on ${port}`))
