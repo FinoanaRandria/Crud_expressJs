271ed701ded0ccfbbb2b334a9b0ad2082819304b
FinoanaRandria <fitiavanarex4@gmail.com>
2023-02-04 16:43:40 +0300

    add finish

--- models/bd.js
+++ models/bd.js
@@ -1,15 +1,29 @@
 const mongoose = require('mongoose');
 
-const mdl = require('./models');
+const model = require('./models');
+
+const insert = async(info)=>{
+     await model.create({
+          nom:info.nom,
+          prenom:info.prenom,
+          telephone:info.telephone
+
+     })
+}
 
 (async ()=>{
    
      try {
           await mongoose.connect("mongodb://localhost/Crud");
           console.log("connected with mongodb")
+
+         
      } catch (error) {
         console.log(message.error)
      }
 
     
 })()
+
+
+module.exports={insert}
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express')
-
+const bodyparser = require('body-parser')
+const {insert} = require('./models/bd')
 const app = express()
 const port = 4333
 
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded())
 app.use("/css",express.static('./public'))
 app.set('view engine', 'ejs')
 app.get('/',(req,res)=>{
     res.render('index')
 })
+app.post("/add",(req,res)=>{
+    const info = req.body
+    insert(info)
+    res.end()
+    console.log(info)
+})
+
 
 app.listen(port,console.log(`Server is runing on ${port}`))
